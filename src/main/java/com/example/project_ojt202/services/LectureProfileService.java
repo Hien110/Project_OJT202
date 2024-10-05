71d8c45e105570cdc72a40302f3a908349a97a0a
Hien110 <minhhien30201@gmail.com>
2024-10-05 18:30:37 +0700

    update Template

--- src/main/java/com/example/project_ojt202/services/LectureProfileService.java
+++ src/main/java/com/example/project_ojt202/services/LectureProfileService.java
@@ -1,8 +1,11 @@
 package com.example.project_ojt202.services;
 
+import org.springframework.stereotype.Service;
+
 import com.example.project_ojt202.models.LectureProfile;
 import com.example.project_ojt202.repositories.LectureProfileRepository;
 
+@Service
 public class LectureProfileService {
     private final LectureProfileRepository lectureProfileRepository;
 
--- src/main/java/com/example/project_ojt202/services/ParentProfileService.java
+++ src/main/java/com/example/project_ojt202/services/ParentProfileService.java
@@ -1,8 +1,11 @@
 package com.example.project_ojt202.services;
 
+import org.springframework.stereotype.Service;
+
 import com.example.project_ojt202.models.ParentProfile;
 import com.example.project_ojt202.repositories.ParentProfileRepository;
 
+@Service
 public class ParentProfileService {
     private final ParentProfileRepository parentProfileRepository;
 
