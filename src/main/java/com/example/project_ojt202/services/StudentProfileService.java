09e740661952709fe7f07ea5e1e9935161d3ef9d
Tran Hoang Anh <thathatha2003@gmail.com>
2024-11-17 23:44:36 +0700

    View All Student List-Admin

--- src/main/java/com/example/project_ojt202/services/StudentProfileService.java
+++ src/main/java/com/example/project_ojt202/services/StudentProfileService.java
@@ -1,20 +1,39 @@
 package com.example.project_ojt202.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.example.project_ojt202.models.Account;
 import com.example.project_ojt202.models.StudentProfile;
+import com.example.project_ojt202.repositories.AccountRepository;
 import com.example.project_ojt202.repositories.StudentProfileRepository;
 
 @Service
 public class StudentProfileService {
     private final StudentProfileRepository studentProfileRepository;
 
-    public StudentProfileService(StudentProfileRepository studentProfileRepository ){
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public StudentProfileService(StudentProfileRepository studentProfileRepository) {
         this.studentProfileRepository = studentProfileRepository;
     }
 
-    public StudentProfile getStudentProfileByStudentID(String studentID){
+    public StudentProfile getStudentProfileByStudentID(String studentID) {
         StudentProfile student = studentProfileRepository.findByStudentID(studentID);
         return student;
+    }
+
+    public Page<StudentProfile> getAllStudents(int page, int size) {
+        return studentProfileRepository.findAll(PageRequest.of(page, size));
+    }
+
+    // Lấy tất cả các tài khoản với accountRole là 'student'
+    public List<Account> getStudentAccounts() {
+        return accountRepository.findByAccountRole("student");
     }
 }
